3e4e2d871225d584adb6a0da8af78032951c95d7
Lawliet <rm87818@estudante.fieb.edu.br>
2022-11-06 21:45:45 -0300

    Funciona

--- Program.cs
+++ Program.cs
@@ -5,25 +5,27 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
-            Funcionario func = new Funcionario();
+            Aluno aluno = new Aluno();
 
-            Console.Write("Nome: ");
-            func.Nome = Console.ReadLine();
-            Console.Write("Salario bruto: ");
-            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Imposto: ");
-            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine();
-            Console.WriteLine("Funcionário: " + func);
+            Console.WriteLine("NOTA FINAL = "
+                + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
 
-            Console.WriteLine();
-            Console.Write("Deseja aumentar o salário em qual porcentagem? ");
-            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            func.AumentarSalario(porcent);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + func);
+            if (aluno.Aprovado()) {
+                Console.WriteLine("APROVADO");
+            }
+            else {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM "
+                    + aluno.NotaRestante().ToString("F2", CultureInfo.InvariantCulture)
+                    + " PONTOS");
+            }
         }
     }
 }
